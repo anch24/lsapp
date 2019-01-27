c8f5cd843685d2f85201e3ad9def01f3afe23855
Raffy Amoguis <me@raffy.com>
2019-01-27 15:51:34 +0800

    first commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app');
+
+@section('content')
+    <a href='/posts' class='btn btn-default'>Go Back</a>
+    <h1>{{$post->title}}</h1>
+    <div>
+        {!!$post->body!!}
+    </div>
+    <hr>
+    <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+    <hr>
+
+    <!-- Checks if its not a guest -->
+    @if(!Auth::guest())
+        <!-- Checks if the user id is equal to the post id -->
+        @if(Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                {!!Form::hidden('_method', 'DELETE')!!}
+                {!!Form::submit('Delete', ['class' => 'btn btn-danger'])!!}
+            {!!Form::close()!!}
+        @endif
+    @endif
+@endsection
